d406de9eda7b2e494faec3633c11eba0f5e49b0c
ravanmaxmud <ravanim@code.edu.az>
2022-07-14 19:48:54 +0400

    Aded Person Class

--- Person-managment/Person-managment/DataBase/Models/Person.cs
+++ Person-managment/Person-managment/DataBase/Models/Person.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace Person_managment.DataBase.Models
 {
-    internal class Person
+     class Person
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FatherName { get; set; }
+        public string Email { get; set; }
+        public string FIN { get; set; }
+        public int Id { get; set; }
+        public static int _idCounter = 1;
+    
+
+        public Person(string firstName ,string lastName, string fatherName,string eMail ,string fin)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            FatherName = fatherName;
+            Email = eMail;
+            FIN = fin;
+            Id = _idCounter;
+            _idCounter++;
+        }
+
     }
 }
