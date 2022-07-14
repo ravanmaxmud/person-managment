6c587f6ba6f97098d645c59db4c4e526c6e7e5ab
unknown <you@example.com>
2022-07-14 08:23:37 -0700

    Added folder

---
+++ Person-managment/Person-managment/UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Person_managment.UI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
