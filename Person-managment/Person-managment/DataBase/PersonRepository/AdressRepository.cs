14006f3f852726bb3131e911dda926fcf539f112
ravanmaxmud <ravanim@code.edu.az>
2022-07-14 20:18:03 +0400

    Aded Adress and Adress Repo

--- Person-managment/Person-managment/DataBase/PersonRepository/AdressRepository.cs
+++ Person-managment/Person-managment/DataBase/PersonRepository/AdressRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Person_managment.DataBase.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,16 @@ using System.Threading.Tasks;
 
 namespace Person_managment.DataBase.PersonRepository
 {
+    
     internal class AdressRepository
     {
+        public static List<Adress> adresses = new List<Adress>();
+        public static Adress AddAdress(string adressName)
+        {
+            Adress adress = new Adress(adressName);
+            adresses.Add(adress);
+            return adress;
+        }
+   
     }
 }
