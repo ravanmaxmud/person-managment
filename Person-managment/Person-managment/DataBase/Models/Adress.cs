aa327fd314447ba9559f2b763a37ec68fa4fab11
ravanmaxmud <ravanim@code.edu.az>
2022-07-14 20:20:49 +0400

    Aded

--- Person-managment/Person-managment/DataBase/Models/Adress.cs
+++ Person-managment/Person-managment/DataBase/Models/Adress.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Person_managment.DataBase.Models
 {
-    internal class Adress
+    class Adress
     {
+       public string AdressName { get; set; }
+        public int AdressId { get; set; }
+
+        public static int _adressIdCounter = 1;
+        public Adress(string adressName)
+        {
+            AdressName = adressName;
+            AdressId = _adressIdCounter;
+            _adressIdCounter++;   
+        }
+
+
     }
 }
